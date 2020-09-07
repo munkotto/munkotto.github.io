373275faa05ba80777626f06bb3ee2773a8c0cdd
munkotto <akselmunken@gmail.com>
2020-09-07 09:23:42 +0200

    checkbox

--- Javascript/BMI.js
+++ Javascript/BMI.js
@@ -1,26 +1,34 @@
 function BMI() {
-    var m = document.getElementById("m ").value;
-    var h = document.getElementById("h ").value;
+    var m = document.getElementById("m").value;
+    var h = document.getElementById("h").value;
+    h = h.replace(/,/g, '.');
+    if (h < 10) {
+        h *= 100;
+    }
     var bmi = m / (h / 100 * h / 100) + 20;
     if (bmi < 38.5) {
-        document.getElementById("resultat ").innerHTML = "Du er tyk, men det kunne være meget værre. Din BMI er: " + bmi.toFixed(2) + ". Det skal nok gå!";
+        document.getElementById("resultat").innerHTML = "Du er tyk, men det kunne være meget værre. Din BMI er: " + bmi.toFixed(2) + ". Det skal nok gå!";
     } else if (bmi >= 38.5 && bmi <= 45) {
-        document.getElementById("resultat ").innerHTML = "Du er tyk. Pas på med maden. Din BMI er: " + bmi.toFixed(2) + ". Du kan stadig redde det!";
+        document.getElementById("resultat").innerHTML = "Du er tyk. Pas på med maden. Din BMI er: " + bmi.toFixed(2) + ". Du kan stadig redde det!";
     } else if (bmi > 25) {
-        document.getElementById("resultat ").innerHTML = "WOW hvor meget spiser du lige?! Din BMI er: " + bmi.toFixed(2) + ". Jeg ville passe noget på hvis jeg var dig!";
+        document.getElementById("resultat").innerHTML = "WOW hvor meget spiser du lige?! Din BMI er: " + bmi.toFixed(2) + ". Jeg ville passe noget på hvis jeg var dig!";
     }
+
 }
 
 function minVegt(val) {
     var vegt = Number(val);
-    document.getElementById("m ").value = vegt;
+    document.getElementById("m").value = vegt;
     document.getElementById("man").width = vegt * 7.5;
     document.getElementById("woman").width = vegt * 7.5;
 }
 
 function minHojde(val) {
     var hojde = Number(val);
-    document.getElementById("h ").value = hojde;
+    document.getElementById("h").value = hojde;
+    if (hojde < 10) {
+        hojde *= 100;
+    }
     document.getElementById("man").height = hojde * 2;
     document.getElementById("woman").height = hojde * 2;
 }
@@ -28,34 +36,45 @@ function minHojde(val) {
 function checkMan() {
     var checkBox = document.getElementById("isMan");
     var picture = document.getElementById("man");
+    var woman = document.getElementById("isWoman")
+    var wamen = 0;
 
     if (checkBox.checked == true) {
         picture.style.display = "block";
     } else {
         picture.style.display = "none";
+    }
+
+    if (checkBox.checked == true && woman.checked == true) {
+        wamen = 1;
+    }
+
+    if (wamen == 1) {
+        woman.checked = false;
+        checkWoman();
+        wamen = 0;
     }
 }
 
 function checkWoman() {
     var checkBox = document.getElementById("isWoman");
+    var man = document.getElementById("isMan")
     var picture = document.getElementById("woman");
+    var men = 0;
 
     if (checkBox.checked == true) {
         picture.style.display = "block";
     } else {
         picture.style.display = "none";
     }
-}
 
-function tjek(check) {
-    var man = document.getElementById("isMan");
-    var woman = document.getElementById("isWoman");
+    if (checkBox.checked == true && man.checked == true) {
+        men = 1;
+    }
 
-    if (man.checked == true && woman.checked == true) {
-        if (check == 1) {
-            woman.checked == false;
-        } else if (check == 2) {
-            man.checked == false;
-        }
+    if (men == 1) {
+        men.checked = false;
+        checkMan();
+        men = 0;
     }
 }
